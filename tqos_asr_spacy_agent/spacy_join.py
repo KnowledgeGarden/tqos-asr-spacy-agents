12700542d1da5a7f6e832d69c83bc36a93f873f2
Marc-Antoine Parent <maparent@acm.org>
2019-12-25 12:00:54 -0500

    wip on joinng streams with Faust

---
+++ tqos_asr_spacy_agent/spacy_join.py
@@ -0,0 +1,40 @@
+import faust
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('join_config.ini')
+assert 'join' in config
+params = config['join']
+target_models = set(params['models'].split(','))
+
+
+app = faust.App(
+    'spacy_join',
+    broker=params['broker'],
+    store=params['store'],
+    value_serializer='json',
+)
+source_topic = app.topic(params['source_topic'])
+dest_topic = app.topic(params['dest_topic'])
+
+table = app.Table(params['aggregate_table'], default=dict)
+
+def analysis_id(model):
+    return "_".join((model["para_info"]["doc_id"], model["para_info"]["para_id"]))
+
+
+@app.agent(source_topic)
+async def join(values):
+    "Collect values until all models are supplied"
+    async for value in values.group_by(analysis_id, name='_key'):
+        id = analysis_id(value)
+        model = value["analyzer"]["name"]
+        aggregate = table[id]
+        aggregate[model] = value
+        if set(aggregate.keys()) == target_models:
+            await dest_topic.send(key=id, value=aggregate)
+            del table[id]
+            print(id, "complete")
+        else:
+            table[id] = aggregate
+            print(id, list(aggregate.keys()))
